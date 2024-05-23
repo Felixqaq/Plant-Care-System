e53715f2f1e7ec4a29f40af1e76c4e7590356e55
mark220620 <uni910620@gmail.com>
2024-05-23 15:39:08 +0800

    新增 控制部分

--- control.py
+++ control.py
@@ -1,11 +1,11 @@
 from DefaultSensor import DefaultSensor
 import RPi.GPIO as GPIO
 
-test = DefaultSensor()
 class control:
-        
+    def __init__(self) :
+        self.sensor = DefaultSensor()
     def LEDswitch(self):
-        if test.read_light == False:
+        if self.sensor.read_light == False:
             return GPIO.output(22,GPIO.HIGH)
         else :
             return GPIO.output(22,GPIO.LOW)
