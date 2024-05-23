4c83330d292d1c2903af0e5c4545a103d0bbbd01
mark220620 <uni910620@gmail.com>
2024-05-23 16:30:57 +0800

    update control

--- control.py
+++ control.py
@@ -1,11 +1,20 @@
 from DefaultSensor import DefaultSensor
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 class control:
     def __init__(self) :
         self.sensor = DefaultSensor()
     def LEDswitch(self):
-        if self.sensor.read_light == False:
-            return GPIO.output(22,GPIO.HIGH)
+        if self.sensor.read_light == 2:
+            return 1
+            
+            #return GPIO.output(22,GPIO.HIGH)
         else :
-            return GPIO.output(22,GPIO.LOW)
+            return 2
+            #return GPIO.output(22,GPIO.LOW)
+    def update(self):
+        return self.LEDswitch()
+
+active =control()
+#while True:
+active.update()
